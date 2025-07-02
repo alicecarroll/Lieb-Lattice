999d8f10b5b972c49c7c6c678fde5f6cac38cde3
alicecarroll <t.tascheta@web.de>
2025-07-02 17:23:47 +0200

    introducing effective interaction (on site only)

---
+++ scModel.py
@@ -0,0 +1,98 @@
+import numpy as np
+
+
+class Model:
+    def __init__(self, **kwargs):
+
+        self.t = kwargs.get('t', 1.0) # Hopping parameter
+        self.mu = kwargs.get('mu', 0.0) # Chemical potential
+
+        # Internal variables are named with _ for convention
+        self.Del = kwargs.get('Del', 0.0)
+        self.Hk = self.HBdG()
+
+    def HBdG(self):
+        """Construct the real space sparse Hamiltonian function."""
+        # Create a sparse matrix representation of the Hamiltonian
+        H = np.zeros((12, 12), dtype=object)  # Placeholder for Hamiltonian matrix
+        
+        for index, _ in np.ndenumerate(H): H[index] = lambda kx, ky, delta: 0
+
+        #h
+        H[np.array([0, 1, 3, 4]), np.array([1, 0, 4, 3])] = lambda kx, ky, delta: -2*self.t * np.cos(kx/2) # Some function of kx, ky
+        H[np.array([1, 2, 4, 5]), np.array([2, 1, 5, 4])] = lambda kx, ky, delta: -2*self.t * np.cos(ky/2) 
+        #h*
+        H[np.array([6, 7, 9, 10]), np.array([7, 6, 10, 9])] = lambda kx, ky, delta: 2*self.t * np.cos(kx/2) # Some function of kx, ky
+        H[np.array([7, 8, 10, 11]), np.array([8, 7, 11, 10])] = lambda kx, ky, delta: 2*self.t * np.cos(ky/2)
+        
+        #Del
+        H[np.array([0,1,2, 6, 7, 8]), np.array([9, 10, 11, 3, 4, 5])] = lambda kx, ky, delta: delta
+        H[np.array([3, 4, 5, 9, 10, 11]), np.array([6, 7, 8, 0, 1, 2])] = lambda kx, ky, delta: -delta
+
+
+        
+        def Hk(kx, ky): 
+            """Evaluate the Hamiltonian at given kx, ky."""
+            hk = np.empty_like(H, dtype=complex)
+            #hk = np.empty_like(H, dtype=float)
+            eps = 1e-15
+            for index in np.ndindex(H.shape): hk[index] = H[index](kx, ky, self.Del)
+            hk[np.abs(hk) < eps] = 0
+
+            return hk
+        
+
+        return Hk
+    
+    def solvHam(self, kx, ky):
+        '''
+        solves hamiltonian for each pair of coordinates
+        '''
+        eps = 1e-15
+        n = np.shape(kx)[0]
+        eig = np.zeros((n, 12))
+    
+        for i in range(n):
+            e = np.linalg.eigvalsh(self.Hk(kx[i], ky[i])) #np.linalg.eig(self.Hk(kx[i], ky[i]))[0]
+            #e[np.abs(e)<eps]=0
+            eig[i]=np.sort(e)
+            
+        return eig.T
+    
+    def Es(self, k):
+        #E=np.array([[],[],[]])
+        l = np.shape(k)[0]
+        E=np.empty((12, l))
+        eps = 1e-15
+
+        for i in k:
+            Erow = self.solvHam(i*np.ones(l), k)
+            E = np.concatenate((E, Erow), axis=1)
+            E[np.abs(E)<eps]=0
+        return E
+    
+    def DOS(self, E, k, sig=5e-2):
+        ''' 
+        returns an array with the density of state values to each E
+
+        E is an array (against which DOS is plotted)
+        En is an array with the E-values for all k grid points of shape (3, ???) because we currently have 3 bands
+        sigma is the width of the gauss function
+        '''
+        def Gauss(E, En, sig):
+            return np.exp(-(E-En)**2/(2*sig**2))
+            
+        En = self.Es(k)
+        b, l = np.shape(En)
+        arr0 = np.ones(l)
+        s1 = 0
+        res = np.ones(np.shape(E)[0])
+        c=0
+        for j in E:
+            for i in range(b):
+                s1 += np.sum(Gauss(j*arr0,En[i], sig))
+            res[c] = s1
+            s1=0
+            c+=1
+        return res
+    
--- sparsemodel.py
+++ sparsemodel.py
@@ -1,6 +1,5 @@
 import numpy as np
 import scipy.sparse as sp
-import random
 
 class SparseModel:
     def __init__(self, **kwargs):
