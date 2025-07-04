d2feaca75c8f71ed325f61d3c06b224252dea861
alicecarroll <t.tascheta@web.de>
2025-07-04 17:07:35 +0200

    effective interaction between nearest neighbors introduced

--- scModel.py
+++ scModel.py
@@ -8,7 +8,12 @@ class Model:
         self.mu = kwargs.get('mu', 0.0) # Chemical potential
 
         # Internal variables are named with _ for convention
-        self.Del = kwargs.get('Del', 0.0)
+        self.Del0 = kwargs.get('Del0', 0.0)
+        self.DelA = kwargs.get('DelA', 0.0)
+        self.DelB = kwargs.get('DelB', 0.0)
+
+        self.dwave = kwargs.get('dwave', False)
+
         self.Hk = self.HBdG()
 
     def HBdG(self):
@@ -16,31 +21,41 @@ class Model:
         # Create a sparse matrix representation of the Hamiltonian
         H = np.zeros((12, 12), dtype=object)  # Placeholder for Hamiltonian matrix
         #H = self.mu*H
-        for index, _ in np.ndenumerate(H): H[index] = lambda kx, ky, delta: 0
+        for index, _ in np.ndenumerate(H): H[index] = lambda kx, ky: 0
 
         #diagonals
-        for i in range(6): H[i,i] = lambda kx, ky, delta: self.mu
-        for i in range(6,12): H[i, i] = lambda kx, ky, delta: - self.mu
+        for i in range(6): H[i,i] = lambda kx, ky: self.mu
+        for i in range(6,12): H[i, i] = lambda kx, ky: - self.mu
         #H[np.array]
         #h
-        H[np.array([0, 1, 3, 4]), np.array([1, 0, 4, 3])] = lambda kx, ky, delta: -2*self.t * np.cos(kx/2) # Some function of kx, ky
-        H[np.array([1, 2, 4, 5]), np.array([2, 1, 5, 4])] = lambda kx, ky, delta: -2*self.t * np.cos(ky/2) 
+        H[np.array([0, 1, 3, 4]), np.array([1, 0, 4, 3])] = lambda kx, ky: -2*self.t * np.cos(kx/2) # Some function of kx, ky
+        H[np.array([1, 2, 4, 5]), np.array([2, 1, 5, 4])] = lambda kx, ky: -2*self.t * np.cos(ky/2) 
         #h*
-        H[np.array([6, 7, 9, 10]), np.array([7, 6, 10, 9])] = lambda kx, ky, delta: 2*self.t * np.cos(kx/2) # Some function of kx, ky
-        H[np.array([7, 8, 10, 11]), np.array([8, 7, 11, 10])] = lambda kx, ky, delta: 2*self.t * np.cos(ky/2)
+        H[np.array([6, 7, 9, 10]), np.array([7, 6, 10, 9])] = lambda kx, ky: 2*self.t * np.cos(kx/2) # Some function of kx, ky
+        H[np.array([7, 8, 10, 11]), np.array([8, 7, 11, 10])] = lambda kx, ky: 2*self.t * np.cos(ky/2)
         
         #Del
-        H[np.array([0,1,2, 6, 7, 8]), np.array([9, 10, 11, 3, 4, 5])] = lambda kx, ky, delta: delta
-        H[np.array([3, 4, 5, 9, 10, 11]), np.array([6, 7, 8, 0, 1, 2])] = lambda kx, ky, delta: -delta
+        #on site
+        H[np.array([0, 1, 2, 9, 10, 11]), np.array([9, 10, 11, 0, 1, 2])] = lambda kx, ky: self.Del0
+        H[np.array([3, 4, 5, 6, 7, 8]), np.array([6, 7, 8, 3, 4, 5])] = lambda kx, ky: -self.Del0
+        #nn AB
+        H[np.array([0, 1, 9, 10]), np.array([10, 9, 1, 0])] = lambda kx, ky: 2*self.DelA*np.cos(kx/2)
+        H[np.array([3, 4, 6, 7]), np.array([7, 6, 4, 3])] = lambda kx, ky: -2*self.DelA*np.cos(kx/2)
+        #nn BC
+        H[np.array([1, 2, 10, 11]), np.array([11, 10, 2, 1])] = lambda kx, ky: 2*self.DelB*np.cos(ky/2)
+        H[np.array([4, 5, 7, 8]), np.array([8, 7, 5, 4])] = lambda kx, ky: -2*self.DelB*np.cos(ky/2)
 
-
+        if self.dwave:
+            self.DelB = -self.DelA 
+        else:
+            self.DelB = self.DelA
         
         def Hk(kx, ky): 
             """Evaluate the Hamiltonian at given kx, ky."""
             hk = np.empty_like(H, dtype=complex)
             #hk = np.empty_like(H, dtype=float)
             eps = 1e-15
-            for index in np.ndindex(H.shape): hk[index] = H[index](kx, ky, self.Del)
+            for index in np.ndindex(H.shape): hk[index] = H[index](kx, ky)
             hk[np.abs(hk) < eps] = 0
 
             return hk
